6532a8a082c04fcf570956d3b0a1439b9c797835
al1211 <kanil013623@gmail.com>
2025-08-27 14:59:06 +0530

    chages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import Dashboard from "./pages/Dashboard";
 import React from "react";
 import ProtectedRoute from "./components/Project.routes.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Singup.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sinup" element={<Signup />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+   <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Navigate to="/login" replace />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/sinup" element={<Signup />} />
+    <Route element={<ProtectedRoute />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+
   );
 }
